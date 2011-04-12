90e4da9e4026aefd5cc9635f1054e033d26aa59a
Kevin Berry <xckjb88@gmail.com>
2011-04-12 02:13:44 -0400

    Added remote code and stack allocation stuff.

--- inject_dso.c
+++ inject_dso.c
@@ -6,17 +6,7 @@
 #include <stdlib.h>
 #include <dlfcn.h>
 #include <signal.h>
-
-/*
- * Important headers:
- * typedef_struct_t mach/i386/_structs.h
- *
- * mach/i386/vm_types.h
- *
- * mach/i386/asm.h
- *
- * mach/i386/ *.h
- * */
+#include <mach-o/loader.h>
 
 void die(const char *s)
 {
@@ -56,29 +46,81 @@ size_t find_dlopen_offset() {
     return offset;
 }
 
+void get_text(char *lib_path)  {
+    DL_info *info;
+    void *handle = dlopen(lib_path, RTLD_LAZY);
+    dladdr(handle, &info);
+    dlclose(handle);
+}
+
 int inject_dso(size_t libsystem_start, size_t dlopen_offset, pid_t pid,
-    char* dso_path) {
+    char* dso_path, char *remote_code, int remote_code_size) {
 
     kern_return_t kret;
     int status;
     mach_port_t task;
-    i386_thread_state_t regs, saved_regs;
+    i386_thread_state_t regs;
     thread_act_port_array_t thread_list;
     mach_msg_type_number_t thread_count;
+    vm_address_t remote_stack_addr = (vm_address_t)NULL;
+    vm_address_t remote_code_addr = (vm_address_t)NULL;
+    vm_address_t remote_dlopen_str_addr = (vm_address_t)NULL;
+    int remote_stack_size = 8192;
 
-    kret = task_for_pid(mach_task_self(), pid, &task);
+    task_for_pid(mach_task_self(), pid, &task);
 
-    if (kret != KERN_SUCCESS)
-        die(mach_error_string(kret));
+    /* Allocate space for the dlopen path string. */
+    vm_allocate(task, &remote_dlopen_str_addr, strlen(dso_path), 1);
+    vm_write(task, remote_dlopen_str_addr, dso_path, strlen(dso_path));
 
+    /* hack. compile and read this from a mach-o object instead of just
+     * hard-coding opcodes etc */
+    char remote_code[] = {
+        0x55,                                     /* push %rbp */
+        0x48, 0x89, 0xe5,                         /* mov %rsp,%rbp */
+        0x48, 0x8b, 0x3d, 0x00, 0x00, 0x00, 0x00, /* mov 0x0(%rip),%rdi */
+        0xbe, 0x01, 0x00, 0x00, 0x00,             /* mov $0x1,%esi */
+        0xe8, 0x00, 0x00, 0x00, 0x00,             /* call <dlopen> */
+        0xc9,                                     /* leaveq */
+        0xc3                                      /* retq */
+    }
+
+    vm_allocate(task, &remote_code_addr, sizeof(remote_code), 1);
+    vm_protect(task, remote_code_addr, remote_code_size, 0, VM_PROT_EXECUTE |
+        VM_PROT_WRITE | VM_PROT_READ);
+
+    /* manually perform relocation for e8 (%rip relative call) to dlopen
+     * offset. */
+    vm_address_t offset = remote_code_addr + 21 + dlopen_offset;
+    remote_code[17] = offset << 24 >> 24;
+    remote_code[18] = offset << 16 >> 16;
+    remote_code[19] = offset >> 16 << 16;
+    remote_code[20] = offset >> 24 << 24;
+
+    /* set relative pointer to dlopen path string to %rdi */
+    //...//
+
+    vm_write(task, remote_code_addr, remote_code, sizeof(remote_code));
+
+    /* Allocate and set up remote stack. */
+    vm_allocate(task, &remote_stack_addr, remote_stack_size, 1);
+    vm_protect(task, remote_stack_addr, remote_stack_size, 0, VM_PROT_WRITE |
+        VM_PROT_READ);
+
+    /* Set the thread state to be set up to call dlopen in the remote
+     * process */
+
+    /* Start the remote thread */
+
+/*
     if (task_suspend(task))
-        die("Can't suspend task.");
+        die("inject 3");
 
     if (task_threads(task, &thread_list, &thread_count))
-        die("Error retrieving thread data.\n");
+        die("inject 4");
 
     if (task_resume(task))
-        die("Can't resume task.");
-
+        die("inject 5");
+*/
     return 0;
 }
