0477a0f3498f562bebbda277a5b9332d6fce48a7
Kevin Berry <xckjb88@gmail.com>
2011-04-21 11:54:47 -0400

    execute remote code.

--- inject_dso.c
+++ inject_dso.c
@@ -49,14 +49,12 @@ size_t find_dlopen_offset() {
 int inject_dso(size_t libsystem_start, size_t dlopen_offset, pid_t pid,
     char* dso_path) {
 
-    int status;
     mach_port_t task;
-    i386_thread_state64_t regs;
+    x86_thread_state64_t regs;
     thread_act_t remote_thread;
     vm_address_t remote_stack_addr = (vm_address_t)NULL;
-    vm_address_t remote_code_addr = (vm_address_t)NULL;
     vm_address_t remote_dlopen_str_addr = (vm_address_t)NULL;
-    size_t remote_stack_contents[3];
+    size_t remote_stack_contents;
     int remote_stack_size = 8192;
 
     task_for_pid(mach_task_self(), pid, &task);
@@ -65,51 +63,26 @@ int inject_dso(size_t libsystem_start, size_t dlopen_offset, pid_t pid,
     vm_allocate(task, &remote_dlopen_str_addr, strlen(dso_path), 1);
     vm_write(task, remote_dlopen_str_addr, (pointer_t)dso_path, strlen(dso_path));
 
-    /* Allocate space for remote code. */
-    char remote_code[] = {
-        0xff, 0x11,         /* call *(%rcx) */
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00
-    };
-
-    vm_allocate(task, &remote_code_addr, sizeof(remote_code), 1);
-    vm_protect(task, remote_code_addr, sizeof(remote_code), 0, VM_PROT_EXECUTE |
-        VM_PROT_WRITE | VM_PROT_READ);
-    vm_write(task, remote_code_addr, (pointer_t)remote_code, sizeof(remote_code));
-
     /* Allocate and set up remote stack. */
     vm_allocate(task, &remote_stack_addr, remote_stack_size, 1);
     vm_protect(task, remote_stack_addr, remote_stack_size, 0, VM_PROT_WRITE |
         VM_PROT_READ);
 
-    remote_stack_contents[0] = (size_t)0x0000DEADBEA7DAD; //Invalid return addr
-    size_t rbp, rsp;
-    rbp = (size_t)remote_stack_addr + (remote_stack_size / 2);
+    remote_stack_contents = 0x0000DEADBEA7DAD; /* return address. */
+    size_t rsp;
     rsp = (size_t)remote_stack_addr + (remote_stack_size / 2);
-    vm_write(task, rbp, (pointer_t)remote_stack_contents, remote_stack_size);
+    vm_write(task, rsp, (pointer_t)remote_stack_contents, remote_stack_size);
 
     /* Set the thread state to be set up to call dlopen in the remote
-     * process */
-    regs.__rcx = (size_t)dlopen_offset;
-    regs.__ebp = (size_t)rbp;
+     * process. Integer and pointer arguments passed in %rdi and %rsi.  */
     regs.__rsp = (size_t)rsp;
     regs.__rdi = (size_t)remote_dlopen_str_addr;
     regs.__rsi = (size_t)RTLD_LAZY;
-    regs.__esi = (size_t)RTLD_LAZY;
-    regs.__rip = (size_t)remote_code_addr;
+    regs.__rip = (size_t)dlopen_offset;
 
     /* Start the remote thread */
     thread_create_running(task, x86_THREAD_STATE64, (thread_state_t) &regs,
-        x86_THREAD_STATE64_COUNT, &remoteThread );
+        x86_THREAD_STATE64_COUNT, &remote_thread);
 
-/*
-    if (task_suspend(task))
-        die("inject 3");
-
-    if (task_threads(task, &thread_list, &thread_count))
-        die("inject 4");
-
-    if (task_resume(task))
-        die("inject 5");
-*/
     return 0;
 }
